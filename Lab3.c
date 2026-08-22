1439e9585b6d3df255c04ee3a6eaf3b93f6388d9
swathi123321 <swathi.23cs172@sode-edu.in>
2026-08-22 09:15:16 +0530

    Create Lab3.c

---
+++ Lab3.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<omp.h>
+int fib(int n){
+    int i,j;
+    if(n<2)
+        return n;
+    else{
+        i=fib(n-1);
+        j=fib(n-2);
+        return i+j;
+    }
+
+        
+}
+int main(){
+    int n;
+    printf("Enter the fibonacci number to calculate:");
+    scanf("%d",&n);
+    omp_set_dynamic(0);
+    omp_set_num_threads(4);
+    {
+        printf("fib(%d) = %d\n",n,fib(n));
+    }
+    return 0;
+    
+}
